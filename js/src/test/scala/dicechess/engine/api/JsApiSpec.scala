290b977e1613d8dbc13f979a11edde6520a87dc2
Jegors Čemisovs <jegors.cemisovs@gmail.com>
2026-05-24 19:28:58 +0300

    test: adjust spacing in getLegalUciMoves promotion test

--- js/src/test/scala/dicechess/engine/api/JsApiSpec.scala
+++ js/src/test/scala/dicechess/engine/api/JsApiSpec.scala
@@ -41,11 +41,11 @@ class JsApiSpec extends FunSuite:
 
   test("getLegalUciMoves: pawn promotion generates UCI moves with promotion pieces") {
     // White pawn on e7, can promote to e8. Dice: [1] (Pawn)
-    val fen = "k7/4P3/8/8/8/8/8/4K3 w - - 0 1"
+    val fen  = "k7/4P3/8/8/8/8/8/4K3 w - - 0 1"
     val dice = js.Array(1)
-    
+
     val moves = JsApi.getLegalUciMoves(fen, dice).toList
-    
+
     // Should generate all 4 promotions for e7e8
     assert(moves.contains("e7e8q"))
     assert(moves.contains("e7e8r"))
