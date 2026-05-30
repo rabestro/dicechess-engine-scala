c0e7914fb7521ed44969e6bf3773bc5a6580a063
Jegors Čemisovs <jegors.cemisovs@gmail.com>
2026-05-30 17:17:42 +0300

    refactor: remove unnecessary whitespace in BotRegistrySpec tests

--- shared/src/test/scala/dicechess/engine/search/BotRegistrySpec.scala
+++ shared/src/test/scala/dicechess/engine/search/BotRegistrySpec.scala
@@ -6,11 +6,11 @@ class BotRegistrySpec extends FunSuite:
 
   test("availableBots returns a list of configured bots sorted by difficulty") {
     val bots = BotRegistry.availableBots
-    
+
     assertEquals(bots.size, 2)
     assertEquals(bots.head.id, "random")
     assertEquals(bots.head.difficulty, 1)
-    
+
     assertEquals(bots(1).id, "greedy")
     assertEquals(bots(1).difficulty, 3)
   }
@@ -18,10 +18,10 @@ class BotRegistrySpec extends FunSuite:
   test("getAlgorithm returns the correct algorithm for a given id (case-insensitive)") {
     assertEquals(BotRegistry.getAlgorithm("random"), Some(RandomSearch))
     assertEquals(BotRegistry.getAlgorithm("RANDOM"), Some(RandomSearch))
-    
+
     assertEquals(BotRegistry.getAlgorithm("greedy"), Some(GreedySearch))
     assertEquals(BotRegistry.getAlgorithm("GrEeDy"), Some(GreedySearch))
-    
+
     assertEquals(BotRegistry.getAlgorithm("unknown"), None)
   }
 
